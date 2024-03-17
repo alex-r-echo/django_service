19503f714ad890d14e4d772c91807fa9a435458d
seal <kjldsf@jlkd.kjls>
2024-03-17 18:14:34 +0500

    Added problems solving of celery parallel tasks.

--- service/services/models.py
+++ service/services/models.py
@@ -2,14 +2,13 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
-from services.tasks import set_price
+from services.tasks import set_price, set_comment
+
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
 
-#    def __str__(self):
-#        return f"Service: {self.name}"
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__full_price = self.full_price
@@ -18,6 +17,8 @@ class Service(models.Model):
         if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
+
         return super().save(*args, **kwargs)
 
 class Plan(models.Model):
@@ -30,8 +31,6 @@ class Plan(models.Model):
     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
-#   def __str__(self):
-#        return f"Plan: {self.plan_type}"
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__discount_percent = self.discount_percent
@@ -40,6 +39,7 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super().save(*args, **kwargs)
 
 
@@ -48,5 +48,5 @@ class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
-    price = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0, max_length='')
     
